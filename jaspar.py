c538b971474b331adfe2a24838133cbfd00a0d94
jlwetzel <jlwetzel@cs.princeton.edu>
2013-07-05 00:01:20 -0400

    2 new functions for jaspar.py -- getNewBuild() and getAnnotsByJasparID()

--- jaspar.py
+++ jaspar.py
@@ -1,12 +1,33 @@
-# Code for dealing with the JASPAR sql_table files and the full matrix file.
+# Code for reteiving and maniupulating the JASPAR sql_table files 
+# and the JASPAR PWM file.
 
-JASPAR_VERSION = '2009-Oct12'
-prefix = '../data/JASPAR/' + JASPAR_VERSION + '/vertebrates/'
+import os
+
+JASPAR_BUILD = '2009-Oct12-NonRedundant'
+
+prefix = '../data/JASPAR/' + JASPAR_BUILD
 protTab = prefix + '/sql_tables/MATRIX_PROTEIN.txt'
 annotTab = prefix + '/sql_tables/MATRIX_ANNOTATION.txt'
 speciesTab = prefix + '/sql_tables/MATRIX_SPECIES.txt'
 matrixTab = prefix + '/sql_tables/MATRIX.txt'
-allMats = prefix + '/matrix_only.txt'
+PWMfile = prefix + '/matrix_only.txt'
+
+def getNewBuild():
+	# Get the latest build of the complete JASPAR CORE set.
+	# First set up directory structure in ../data/JASPAR/
+	JASPAR_HTML_PREFIX = "http://jaspar.genereg.net//" + \
+		"html/DOWNLOAD/jaspar_CORE/non_redundant/all_species/"
+	sqlTables = ["MATRIX.txt", "MATRIX_ANNOTATION.txt", "MATRIX_DATA.txt",
+			 	"MATRIX_PROTEIN.txt", "MATRIX_SPECIES.txt"]
+
+	os.mkdir("../data/JASPAR/" + JASPAR_BUILD)
+	os.mkdir("../data/JASPAR/" + JASPAR_BUILD + "/sql_tables")
+	for tab in sqlTables:
+		os.system("wget -P " + prefix + "/sql_tables/ " + 
+		          JASPAR_HTML_PREFIX + "/sql_tables/" + tab) 
+	os.system("wget -P " + prefix + " " + JASPAR_HTML_PREFIX 
+	          + "matrix_only/matrix_only.txt") 
+
 
 def getIDsByAnnot(annot, currentList = None):
 	# Returns a list of JASPAR unique IDs that are are
@@ -28,6 +49,8 @@ def getIDsByAnnot(annot, currentList = None):
 			ids.add(sp_line[0])
 
 	annotFile.close()
+	ids = list(ids)
+	ids = [int(i) for i in ids]
 	return sorted(list(ids))
 
 def JASPARIDs2proteinIDs(JASPARids):
@@ -39,7 +62,7 @@ def JASPARIDs2proteinIDs(JASPARids):
 	proteinIDs = []
 	for line in protFile:
 		sp_line = line.strip().split()
-		if sp_line[0] == JASPARids[i]:
+		if int(sp_line[0]) == JASPARids[i]:
 			proteinIDs.append(sp_line[1])
 			i += 1
 		if i == len(JASPARids):
@@ -48,11 +71,40 @@ def JASPARIDs2proteinIDs(JASPARids):
 	protFile.close()
 	return proteinIDs
 
+def getAnnotsByJASPARid(JASPARids, label):
+	# Finds the annotation associated with the JasparID
+	# and label for each ID in the ***SORTED*** 
+	# list of sorted JASPARids	
+	annotFile = open(annotTab, 'r')
+	i = 0
+	vals = []
+	
+	for line in annotFile:
+		if len(line) != 0:
+			sp_line = line.strip().split('\t')
+		if int(sp_line[0]) > JASPARids[i]:
+			print "No label: %s for JASPAR id %d" %(label, JASPARids[i])
+			i += 1
+			if i == len(JASPARids):
+				break
+		if int(sp_line[0]) == JASPARids[i] and sp_line[1] == label:
+			vals.append(sp_line[2])
+			i += 1
+		if i == len(JASPARids):
+			break
+
+	annotFile.close()
+	return vals
+
 def main():
+	#getNewBuild()
 	JASPARids = getIDsByAnnot(('family', 'BetaBetaAlpha-zinc finger'))
-	protIDs = JASPARIDs2proteinIDs(JASPARids)
-	for ID in protIDs:
-		print ID
+	print JASPARids
+	x = getAnnotsByJASPARid(JASPARids, "family")
+	#protIDs = JASPARIDs2proteinIDs(JASPARids)
+	#print(len(protIDs))
+	for t in x:
+		print t
 
 if __name__ == '__main__':
 	main()
