679957820fbb412b334ff48e5860d0cdfb5356de
jlwetzel <jlwetzel@cs.princeton.edu>
2013-10-03 18:20:18 -0400

    Tried to see if easy way to remove bumps at ~<0 for high-low stringency comparisons

--- figurecode/contextAnalysis.R
+++ figurecode/contextAnalysis.R
@@ -1,9 +1,9 @@
 library(ggplot2)
 
 # Create the list of directories
-fings <- c('F1', 'F2', 'F3')
+fings <- c("F2")#c('F1', 'F2', 'F3')
 strins <- c('low', 'high')
-filts <- c('filt_10e-4_025_0_c', 'filt_10e-4_05_0')
+filts <- c("filt_10e-4_025_0_c")#c('filt_10e-4_025_0_c', 'filt_10e-4_05_0')
 inPref <- '../../data/b1hData/antonProcessed'
 outDir <- '../../figures/contextAnalysis'
 inFiles <- vector()
@@ -24,14 +24,14 @@ dfList <- list()
 for (i in 1:length(inFiles)) {
   dfList[[outTags[i]]] <- read.table(inFiles[i], sep = '\t',
                                      header = TRUE)
-  g <- ggplot(dfList[[outTags[i]]], aes(entropy)) +
-    geom_histogram() +
-    xlab("Entropy") +
-    ylab("Number of Canonical Proteins")
-  plotFile <- paste(outDir, 
-                    paste0("entropy_", outTags[i], '.pdf'),
-                    sep = '/')
-  ggsave(plotFile, plot = g)
+#  g <- ggplot(dfList[[outTags[i]]], aes(entropy)) +
+#    geom_histogram() +
+#    xlab("Entropy") +
+#    ylab("Number of Canonical Proteins")
+#  plotFile <- paste(outDir, 
+#                    paste0("entropy_", outTags[i], '.pdf'),
+#                    sep = '/')
+#  ggsave(plotFile, plot = g)
 }
 
 # Compare high and low stringency overlap
@@ -44,22 +44,30 @@ for (f in fings)
     # high vs low stringency
     interProts <- intersect(dfList[[lowTag]]$prot,
                             dfList[[highTag]]$prot)
+    
     pccs <- vector()
+    freqs <- vector()
+    freqsNegCorr <- vector()
     for (p in interProts) {
-      v1 <- as.numeric(dfList[[lowTag]][dfList[[lowTag]]$prot == p,])
-      v1 <- v1[3:length(v1)]
-      v2 <- as.numeric(dfList[[highTag]][dfList[[highTag]]$prot == p,])
-      v2 <- v2[3:length(v2)]
+      v1Vals <- as.numeric(dfList[[lowTag]][dfList[[lowTag]]$prot == p,])
+      v1 <- v1Vals[4:length(v1Vals)]
+      v2Vals <- as.numeric(dfList[[highTag]][dfList[[highTag]]$prot == p,])
+      v2 <- v2Vals[4:length(v2Vals)]
+      freqs <- c(freqs, v1Vals[3])
+      pcc <- cor(v1, v2)
       pccs <- c(pccs, cor(v1, v2))
+      if (pcc < 0) {
+        freqsNegCorr <- c(freqsNegCorr, v1Vals[3])
+      }
     }
     
     pccFrame <- data.frame(prot = interProts, pcc = pccs)
     plotFile <- paste("intersectHighLow", "PCC",
                      f, s, filt, sep = '_')
     plotFile <- paste0(outDir, '/', plotFile, '.pdf')
-    g <- ggplot(pccFrame, aes(pcc)) + 
-      geom_histogram() +
-      xlab("PCC between high and low") +
-      ylab("Number of Canonical Proteins")
-    ggsave(plotFile, plot = g)
+    #g <- ggplot(pccFrame, aes(pcc)) + 
+    #  geom_histogram() +
+    #  xlab("PCC between high and low") +
+    #  ylab("Number of Canonical Proteins")
+    #ggsave(plotFile, plot = g)
   }
